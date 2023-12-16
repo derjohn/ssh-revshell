99450ccfb56e9fee8f6d5466c13f366112ac56fc
derjohn <himself@derjohn.de>
2023-12-16 22:33:09 +0100

    Make revshell.sh more resilient.

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
+PATHPREFIX?=
+
 help:
 	@awk 'BEGIN {FS = ":.*##"; printf "Usage: make \033[36m<target>\033[0m\n"} /^[0-9a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
+copy: ## copies the files PATHPREFIX=
+	sudo cp -p revshell.sh $(PATHPREFIX)/usr/local/sbin
+	sudo cp -p systemd-unit-service-reverseshell $(PATHPREFIX)/lib/systemd/system/revshell.service
+	sudo cp -p etc-revshell $(PATHPREFIX)/etc/revshell
+
 install: ## Installs and enabled the systemd service and the config file in /etc/revshell
-	sudo cp -p revshell.sh /usr/local/sbin
-	sudo cp -p systemd-unit-service-reverseshell /lib/systemd/system/revshell.service
-	sudo cp -p etc-revshell /etc/revshell
+	make copy
 	sudo systemctl daemon-reload
 	sudo systemctl enable --now revshell.service
+
+enable-by-symlink:
+	ln -sf /lib/systemd/system/revshell.service /etc/systemd/system/default.target.wants/revshell.service
 
--- revshell.sh
+++ revshell.sh
@@ -3,19 +3,21 @@
 . /etc/revshell
 
 IFCE=${IFACE:-$(ip --oneline ro get 8.8.8.8 | cut -f 5 -d ' ')}
-MAC=$(cat /sys/class/net/${IFCE}//address)
+MAC=$(cat /sys/class/net/${IFCE}/address)
 HST=$(echo ${MAC} | tr ':' '-')
 FQDN=${HST}.${ENDPOINT}
-LISTENIP=$(dig +short ${FQDN})
+LISTENIPDEFNS=$(dig +short ${FQDN} 2> /dev/null | grep -v ';;')
+LISTENIP8888=$(dig @8.8.8.8 +short ${FQDN} 2> /dev/null | grep -v ';;')
+LISTENIP=${LISTENIP8888:-${LISTENIPDEFNS}}
 
 umask 0077
-cat <<EOF > ~/.ssh/known_hosts.revshell
+cat <<EOF > /root/.ssh/known_hosts.revshell
 ${ENDPOINTHOSTKEY}
 EOF
 
-cat <<EOF > ~/.ssh/privkey.revshell
+cat <<EOF > /root/.ssh/privkey.revshell
 ${SSHPRIVKEY}
 EOF
 
-/usr/bin/ssh -vvv -g -N -T -o "ServerAliveInterval 10" -o "ExitOnForwardFailure yes" -o "UserKnownHostsFile ~/.ssh/known_hosts.revshell" -i ~/.ssh/privkey.revshell -R ${LISTENIP}:${ENDPOINTPORT}:localhost:${LOCALSSHPORT} ${ENDPOINTUSR}@${ENDPOINT}
+/usr/bin/ssh -v -g -N -T -o "ServerAliveInterval 10" -o "ExitOnForwardFailure yes" -o "UserKnownHostsFile /root/.ssh/known_hosts.revshell" -i /root/.ssh/privkey.revshell -R ${LISTENIP}:${ENDPOINTPORT}:localhost:${LOCALSSHPORT} ${ENDPOINTUSR}@${ENDPOINT}
 
